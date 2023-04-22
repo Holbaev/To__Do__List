da37ed419290a1ebcbac184854cccf9400735e26
Aiba <holbinaiba@gmail.com>
2023-04-22 17:27:53 +0600

    feat: prepare js file

---
+++ script.js
@@ -0,0 +1,4 @@
+const taskInput = document.querySelector(".input");
+const addButton = document.querySelector(".add");
+const currentItems = document.querySelector(".list__item-current-task");
+const completedItems = document.querySelector(".list__item-completed-task");
