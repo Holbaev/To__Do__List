d237137352ff593d4e90ee6b4a572ac437f71235
Aiba <holbinaiba@gmail.com>
2023-04-22 17:32:37 +0600

    feat: add addTask function

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ const taskInput = document.querySelector(".input");
 const addButton = document.querySelector(".add");
 const currentItems = document.querySelector(".list__item-current-task");
 const completedItems = document.querySelector(".list__item-completed-task");
+
+addButton.addEventListener("click" , () =>{
+    addTask();
+});
+
+function addTask(){
+    //create task div
+    const task = document.createElement("div");
+    task.innerHTML = `
+                    <input type="checkbox">
+                    <h1></h1>
+                    <button>Edit</button>
+                    <button>Delete</button>`;
+    task.classList.add("task-box");
+    // task-check
+    const task__check = task.children[0];
+    // task-text
+    const task__text = task.children[1];
+    task__text.classList.add("task-text");
+    // editButton
+    const editButton = task.children[2];
+    editButton.classList.add("edit");
+    // deleteButton 
+    const deleteButton = task.children[3];
+    deleteButton.classList.add("delete");
+};
